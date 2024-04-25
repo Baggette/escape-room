1414de3ce2b02c704af733655fae606032e15870
baggette <58569691+Baggette@users.noreply.github.com>
2024-04-25 09:58:22 -0500

    gaming
    gamer

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.IO;
+
+namespace escape_room;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //not 100% sure if this is needed, could probably just use a method instead of a constructor
+            var postApi = new postApi();
+            //store the data in a constructor (at least thats what I think its called I dunno its late)
+            string secret_key = File.ReadAllText(@"G:\comp sci 12\webapi test\api.txt");
+            Console.WriteLine("Please enter a prompt");
+            string prompt = Console.ReadLine();
+            var data = new Data { api_key = secret_key, prompt = $"{prompt}", gemini = true };
+
+            //api endpoint
+            var url = "https://something-lfu0.onrender.com";
+            //execute the thing that does the post request
+            var task = postApi.SendData(url, data);
+            //set the data in a variable
+            var response = task.Result;
+
+            Console.WriteLine(response);
+            //print to console what the output was
+
+        }
+    }
---
+++ apiEndpoint.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace escape_room
+{
+    //this class stores the stuff 
+    public class Data
+    {
+        public string api_key { get; set; }
+        public string prompt { get; set; }
+        public bool gemini { get; set; }
+    }
+
+    //create a new class where the post requests will be executed from
+    public class postApi
+    {
+
+        //from what I can gather this just creates a new instance of HttpClient in preparation for doing the request
+        private readonly HttpClient _client;
+
+        public postApi()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<string> SendData(string url, Data data)
+        {
+            //convert the data from the Data class into js
+            var jsonData = JsonSerializer.Serialize(data);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            //actually send the request now and return the response back to where it was executed
+            HttpResponseMessage response = await _client.PostAsync(url, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                //convert returned data to string
+                string json = await response.Content.ReadAsStringAsync();
+                //return the data back to where it was executed
+                return json;
+            }
+            else
+            {
+                //temp probably if there was nothing
+                return "nothing";
+            }
+        }
+
+    }
+}
