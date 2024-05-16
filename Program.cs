e62079604d69b7bff083a9171e80d89e97764983
BrainRotted <omariog@live.wsd1.org>
2024-05-16 13:57:59 -0500

    stevejobs
    time apple

--- Program.cs
+++ Program.cs
@@ -9,12 +9,28 @@ using System.Text.Json.Serialization;
 using System.Collections.Generic;
 using System.IO;
 
+using file_thing;
+
 namespace escape_room;
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            file_stuff filestuff = new file_stuff();
+
+
+           string fileHash = filestuff.writeToFileAndHash("what are you doing here lmao");
+           string input = Console.ReadLine();
+           string hased_input = filestuff.HashFile(input);
+
+           if(hased_input == fileHash)
+           {
+                Console.WriteLine("you did it :D");
+           }
+           
+
+            /*
             //not 100% sure if this is needed, could probably just use a method instead of a constructor
             var postApi = new postApi();
             //store the data in a constructor (at least thats what I think its called I dunno its late)
@@ -30,8 +46,7 @@ namespace escape_room;
             //set the data in a variable
             var response = task.Result;
 
-            Console.WriteLine(response);
-            //print to console what the output was
-
+            
+            //print to console what the output was*/
         }
     }
